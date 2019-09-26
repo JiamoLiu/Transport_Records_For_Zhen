7ab09af8090dc9e3511abff2e9f7f5ff96f96f3d
Jiamo Liu <Jiamo.Liu95@gmail.com>
2019-09-27 03:35:38 +0800

    Done with V1

--- Driver.java
+++ Driver.java
@@ -6,17 +6,8 @@ import java.io.FileReader;
 import java.io.FileWriter;
 
 import java.io.IOException;
-import java.io.Writer;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.time.DayOfWeek;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.Month;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Dictionary;
-import java.util.List;
 import java.util.Scanner;
 
 
@@ -33,18 +24,18 @@ public class Driver {
     private static String PersonalInfoFile = "PersonalInfo";
     private static String PersonalRecordFile = "PersonalRecord";
 
-    private static String PersonalRecordHeader = "Date,Time,From,To,People,Purpose,Driver";
+    private static String PersonalRecordHeader = "Date,Time,From,To,Purpose,Driver,People";
     private static Scanner scan = new Scanner(System.in);
 
     public static void main(String[] args) throws Exception {
         System.out.println("欢迎使用行车记录仪2.0");
         while(true)
         {
-            System.out.println("A-智能填写，B-手动填写，C-添加志愿者，D-删除志愿者，E-查看志愿者, Z-退出");
+            System.out.println("A-智能填写，B-手动填写，C-添加志愿者，D-删除志愿者，E-查看志愿者, F-添加志愿者信息, Z-退出");
             InitialiseSystem();
             String Input = GetUserInput();
             if (Input.equalsIgnoreCase("A")) {
-
+                AssistFillIn();
             }
 
             if(Input.equalsIgnoreCase("B"))
@@ -65,6 +56,11 @@ public class Driver {
             {
                 ListVolunteers();
             }
+
+            if(Input.equalsIgnoreCase("F"))
+            {
+                AddInfoToVolunteer();
+            }
     
             if(Input.equalsIgnoreCase("Z"))
             {
@@ -75,16 +71,185 @@ public class Driver {
 
     }
 
+    private static void AddInfoToVolunteer() throws IOException {
+        System.out.println("志愿者的名字是什么？");
+        String name = GetUserInput();
+        if(!VolunteerExists(name))
+        {
+            System.out.println("没有找到志愿者");
+            return;
+        }
+        System.out.println("信息格式为：DayOfWeek,Time,From,To,Purpose,Driver");
+        String input = GetUserInput();
+        if (input.split(",").length != 6)
+        {
+            System.out.println("输入格式不对，重新输入");
+            return;
+        }
+        AppendToCSVFile(GetPersonalCSVFilePath(name, PersonalInfoFile), input);
+    }
+
+    private static void AssistFillIn() throws Exception {
+        ArrayList<String> names = GetVolunteerNames();
+        if(names == null)
+        {
+            System.out.println("没有志愿者记录");
+            return;
+        }
+        for(int i = 0; i<names.size();i++)
+        {
+            AssistFillInFor(names.get(i));
+        }
+    }
+
+    private static void AssistFillInFor(String name) throws Exception {
+        LocalDate RecordingDate = GetLatestDateForVolunteer(name);
+        if(RecordingDate == null)
+        {
+            System.out.println(name+"并没有记录在系统里，请手动为他填写一个记录");
+            return;
+        }
+        RecordingDate = GetNextWorkingDay(RecordingDate);
+        
+        while(RecordingDate.isBefore(LocalDate.now()) || RecordingDate.isEqual(LocalDate.now()))
+        {
+            LocalDate LatestDate = GetLatestDateForVolunteer(name);
+            System.out.println(name + " 上次的行程记录于" + LocalDateToString(LatestDate) + " "+LatestDate.getDayOfWeek());
+            LocalDate WorkingDay = GetNextWorkingDay(LatestDate);
+            String PersonalInfo = GetPersonalInfo(name, WorkingDay);
+            if(PersonalInfo == null)
+            {
+                System.out.println(name+" 今天没有行程信息，按C跳过今天，按B手动填写, Z-退出");              
+                String input = GetUserInput();
+                if(input.equalsIgnoreCase("C"))
+                {
+                    UpdateLastestDate(name, RecordingDate);
+                    RecordingDate = RecordingDate.plusDays(1);
+                }
+                if(input.equalsIgnoreCase("B"))
+                {
+                    ManualFillIn(name);
+                    RecordingDate = RecordingDate.plusDays(1);
+                }
+                if(input.equalsIgnoreCase("Z")){break;}
+            }
+            else
+            {
+                System.out.println("It is "+LocalDateToString(RecordingDate)+" " + RecordingDate.getDayOfWeek() + " PersonalInfo: " + PersonalInfo);
+                String tempRes = GetTempResultStringForAutoFill(PersonalInfo,name, RecordingDate);
+                System.out.println("智能分析出结果：" + tempRes); 
+                System.out.println("A-智能填写，C-跳过今天，Z-退出");
+                
+                String input = GetUserInput();
+                if(input.equalsIgnoreCase("A"))
+                {
+                    AppendToPersonalRecord(name, tempRes);
+                    AppendToAggregateRecords(name, tempRes);
+                    UpdateLastestDate(name, RecordingDate);
+                    RecordingDate = RecordingDate.plusDays(1);
+                }
+                if(input.equalsIgnoreCase("C"))
+                {
+                    UpdateLastestDate(name, RecordingDate);
+                    RecordingDate = RecordingDate.plusDays(1);                
+                }
+                if(input.equalsIgnoreCase("Z"))
+                {
+                    break;
+                }
+            }
+
+        }
+
+        
+
+
+    }
+
+
+
+    private static String GetTempResultStringForAutoFill(String personalInfo, String name, LocalDate recordingDate) {
+        String[] data = personalInfo.split(",");
+        String[] res = new String[data.length + 1 ];
+        res[0] = LocalDateToString(recordingDate);
+
+        for(int i = 1; i<res.length;i++)
+        {
+            if(i == res.length - 1)
+            {
+                res[i] = name;
+                break;
+            }
+
+            res[i] = data[i];
+        }
+        return String.join(",", res);
+    }
+
+    private static String GetPersonalInfo(String name, LocalDate workingDay) throws IOException {
+        ArrayList<String> PersonalInfo = ReadLinesFromFile(GetPersonalCSVFilePath(name, PersonalInfoFile));
+        if(PersonalInfo.size() == 1)
+        {
+            return null;
+        }
+        for(int i = 1;i<PersonalInfo.size();i++)
+        {
+            String row = PersonalInfo.get(i);
+            String[] data = row.split(",");
+            if(data[0].equals(String.valueOf(workingDay.getDayOfWeek().getValue())))
+            {
+                return row;
+            }
+        }
+        return null;
+        
+    }
+
+    private static LocalDate GetNextWorkingDay(LocalDate latestDate) {
+        LocalDate temp = latestDate;
+        while(true)
+        {
+            temp = temp.plusDays(1);
+            if(!IsWeekend(temp))
+            {
+                return temp;
+            }
+        }
+    }
+
+    private static LocalDate GetLatestDateForVolunteer(String name) throws Exception {
+        ArrayList<String> dateStrs = ReadLinesFromFile(GetPersonalCSVFilePath(name, LastestVolunteerDateFile));
+        if(dateStrs.size() == 1)
+        {return null;}
+
+        return GetDateFromUserString(dateStrs.get(1));
+
+    }
+
+    private static ArrayList<String> GetVolunteerNames() throws IOException {
+        ArrayList<String> names =ReadLinesFromFile(GetCSVFileName(VolunteerNameFile));
+        if(names.size() == 1)
+        {return null;}
+        names.remove(0);
+        return names;
+    }
+
     private static void ManualFillIn() throws Exception {
         System.out.println("志愿者的名字是什么？");
         String name = GetUserInput();
+        ManualFillIn(name);
+    }
+
+    private static void ManualFillIn(String name) throws IOException, Exception {
         if(!VolunteerExists(name))
         {
             System.out.println("没有这个志愿者");
             return;
         }
-        System.out.println("请输入他的行程，格式为：Date(2019-01-01),Time(15:30),From,To,People(Saparate with &),Purpose,Driver");
+        System.out.println("请输入他的行程，格式为：Date(2019-01-01),Time(15:30),From,To,Purpose,Driver,People(Saparate with &), Z键返回");
         String Input = GetUserInput();
+        if(Input.equalsIgnoreCase("Z"))
+        {return;}
         String[] data = Input.split(",");
         LocalDate date = GetDateFromUserString(data[0]);
         if(IsWeekend(date))
@@ -96,8 +261,6 @@ public class Driver {
         AppendToAggregateRecords(name, Input);
         UpdateLastestDate(name,date);
     }
-
-    //Date,Time,From,To,People,Purpose,Driver
 
     private static void UpdateLastestDate(String VolunteerName, LocalDate date) throws IOException {
         ArrayList<String> file = ReadLinesFromFile(GetPersonalCSVFilePath(VolunteerName, LastestVolunteerDateFile));
@@ -135,7 +298,7 @@ public class Driver {
             int DaysTillFriday = 5 - date.getDayOfWeek().getValue();
             LocalDate start = date.minusDays(DaysFromMonday);
             LocalDate end = date.plusDays(DaysTillFriday);
-            return GetCSVFileName(LocalDateToString(start) + " "+ LocalDateToString(end));
+            return GetCSVFileName(LocalDateToString(start) + " to "+ LocalDateToString(end));
             
         }
         catch(Exception e)
